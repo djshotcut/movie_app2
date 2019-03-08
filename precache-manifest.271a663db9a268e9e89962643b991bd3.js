672aa04dda0ce776dd28de8ee2b552a0e2312ef5
djshotcut <djshotcut@naver.com>
2019-03-08 17:36:22 +0900

    Updates

---
+++ precache-manifest.271a663db9a268e9e89962643b991bd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f0e4f2c6609524639bc",
+    "url": "/movie_app2/static/js/runtime~main.2f0e4f2c.js"
+  },
+  {
+    "revision": "eb3ccb8e09da1450a7ef",
+    "url": "/movie_app2/static/js/main.eb3ccb8e.chunk.js"
+  },
+  {
+    "revision": "79876ddfb540893b229e",
+    "url": "/movie_app2/static/js/2.79876ddf.chunk.js"
+  },
+  {
+    "revision": "eb3ccb8e09da1450a7ef",
+    "url": "/movie_app2/static/css/main.e05b52ea.chunk.css"
+  },
+  {
+    "revision": "e375df2888f6f3630a683217e3e21fa4",
+    "url": "/movie_app2/index.html"
+  }
+];
